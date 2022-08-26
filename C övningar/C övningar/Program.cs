6be9fbf62c02e6ca72d02b5c5ce5e39a3fda13ac
EmilWallman <emil.wallman@elev.ga.ntig.se>
2022-08-26 14:27:43 +0200

    test

---
+++ C övningar/C övningar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_övningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int go = 0;
+            Console.WriteLine("insert number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2 + num3;
+            Console.WriteLine("Här är summan av talen: " + sum);
+         
+        }
+    }
+}
