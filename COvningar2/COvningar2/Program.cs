7bb2d899f888cbcaf2e9042d53798f45a4299633
EmilWallman <emil.wallman@elev.ga.ntig.se>
2022-08-31 09:33:21 +0200

    övning 41

---
+++ COvningar2/COvningar2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace COvningar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum;
+
+            Console.WriteLine("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter another number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                sum = Math.Pow(num1, 4);
+            }
+
+            else
+            {
+                sum = num1 + num2;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
